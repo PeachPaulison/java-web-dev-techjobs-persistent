5e71f6fa6005a87f0c5326c9116bb7ab068f4f98
Peach Paulison <dlpeachpaulison@gmail.com>
2021-12-12 18:23:27 -0500

    Completed Part 4 up to test SQL.  Passing all of TestTaskFour up to SQL.

--- queries.sql
+++ queries.sql
@@ -7,8 +7,7 @@ WHERE location = "St. Louis City";
 -- Part 3: Test it with SQL
 DROP TABLE job;
 -- Part 4: Test it with SQL
-"SELECT\\s+\\*\\s+FROM\\s+skill" +
-                "\\s*(LEFT|INNER)?\\s+JOIN\\s+job_skills\\s+ON\\s+(skill.id\\s+=\\s+job_skills.skills_id|job_skills.skills_id\\s+=\\s+skill.id)" +
-                "(\\s*WHERE\\s+job_skills.jobs_id\\s+IS\\s+NOT\\s+NULL)?" +
-                "\\s*ORDER\\s+BY\\s+name\\s+ASC;"
 SELECT * FROM skill
+LEFT JOIN job_skills ON skill_id = job_skills.skills_id
+WHERE job_skills.jobs.id IS NOT NULL
+ORDER BY name ASC;
