eb4e1c3d1f47f400053b5884f88f20ed39bcfa5e
Peach Paulison <dlpeachpaulison@gmail.com>
2021-12-12 20:10:54 -0500

    Completed.  Passing all tests.  (Last test of SQL Query buggy?)

--- queries.sql
+++ queries.sql
@@ -7,7 +7,9 @@ WHERE location = "St. Louis City";
 -- Part 3: Test it with SQL
 DROP TABLE job;
 -- Part 4: Test it with SQL
-SELECT * FROM skill
-LEFT JOIN job_skills ON skill_id = job_skills.skills_id
-WHERE job_skills.jobs.id IS NOT NULL
-ORDER BY name ASC;
+SELECT * FROM skill INNER JOIN job_skills ON job_skills.skills_id = skill.id ORDER BY name ASC;
+
+--SELECT * FROM skill
+--LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+--WHERE job_skills.job.id IS NOT NULL
+--ORDER BY name ASC;
